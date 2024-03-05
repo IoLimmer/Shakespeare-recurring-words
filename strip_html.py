5297e32351a417435758dfc62145a62bc6eade07
Io Limmer <jh20006@bristol.ac.uk>
2024-03-05 17:30:42 +0000

    Push

--- strip_html.py
+++ strip_html.py
@@ -1,12 +1,46 @@
-import re
+import re, os
 
-with open("macbeth.txt", "r") as file:
+import utils
+
+
+with open(os.path.join(utils.DIRECTORY, "macbeth.txt"), "r") as file:
     html_str = file.read()
-with open("macbeth.txt", "r") as file:
+with open(os.path.join(utils.DIRECTORY, "macbeth.txt"), "r") as file:
     html_list = file.readlines()
 
 
-with open("macbeth2.txt", "w") as file:
-    pattern = "<(.|\n)*?>"
+with open(os.path.join(utils.DIRECTORY, "macbeth2.txt"), "w") as file:
+
+    ### Anything marked in italics is a stage direction, so not important to this
+    pattern = "<i>.*?</i>"
     html_str = re.sub(pattern, "", html_str)
+
+    ### Upper case words are assumed to be the names of characters
+    pattern = "\n<A NAME=speech.*?/a>"
+    html_str = re.sub(pattern, "", html_str)
+
+    ### Anything marked which h3 is text reading the act and scene number
+    pattern = "\n<[Hh]3.*?/[Hh]3>"
+    html_str = re.sub(pattern, "", html_str)
+
+
+    pattern = "<(.|\n)*?>"
+    html_str = re.sub(pattern, "", html_str) ### remove html artifacts
+    html_str = re.sub("\n+", " ", html_str) ### replace newlines with spaces
+
+
+
+    ### Remove all punctuation except apostrophes 
+    html_str = re.sub(r"[^a-zA-Z0-9\'\s]",'',html_str)
+
+
+    ### remove everything before "Entire play"
+    start_index = html_str.find("Entire play") + len("Entire play") + 1
+    # print(start_index)
+    html_str = html_str[start_index:]
+
+    # html_str = re.sub(r"\b[A-Z]{2,}\b",'',html_str) ### Remove anything in block capitals
+    html_str = re.sub(" +", " ", html_str) ### replace excessive spaces with just one
+
     file.write(html_str)
+
